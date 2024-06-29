ebf82a0bdbb4da74f4b6bf755b27841a7f6d9566
tahyo27 <tahyo27@gmail.com>
2024-06-29 14:03:09 +0900

    init 테스트

---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script setup>
+import './../assets/home.css';
+</script>
+
+<template>
+  <div>
+    <header class="header">
+      <span>EVERYTHING IS PERSONAL. INCLUDING THIS BLOG.</span>
+      <h1>Train of Thought</h1>
+      <nav class="nav">
+        <div>Home</div>
+        <div>About</div>
+        <div>My Blog</div>
+        <div>Contact</div>
+        <div>Search</div>
+        <div>SNS</div>
+      </nav>
+    </header>
+  <main class="main">
+    <div class="feature">FEATURED POST</div>
+    <div>
+      <h1>RAIN OF THOUGHT</h1>
+      <ul>
+        <li>블로그 포스트 목록</li>
+        <li>블로그 포스트 목록</li>
+        <li>블로그 포스트 목록</li>
+        <li>블로그 포스트 목록</li>
+        <li>블로그 포스트 목록</li>
+        <li>블로그 포스트 목록</li>
+        <li>블로그 포스트 목록</li>
+        <li>블로그 포스트 목록</li>
+      </ul>
+    </div>
+  </main>
+  <footer class="footer">
+    내가 제작했습니다
+
+  </footer>
+  </div>
+</template>
+
