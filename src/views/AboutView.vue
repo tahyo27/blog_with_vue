e179413f77f3c4e7fad951af73883fb9a215eada
tahyo27 <tahyo27@gmail.com>
2024-09-15 20:54:10 +0900

    about me 페이지 완성

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <div>마이블로그 형식 생각 두줄 네모 형식으로 만들고</div>
-    <div>누르면 해당 페이지로 가게</div>
+    <div class="about-content">
+      <div><img src="../../public/고라파덕.webp"></div>
+      <h1>Hey! So Glad You're Here.</h1>
+      <div><p>TH의 블로그입니다. java, spring, mysql, vue3등을 사용해 블로그를 만들면서 공부하는 장소입니다.</p></div>
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+  .about {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+  }
 
+  .about-content {
+    text-align: center;
+  }
 </style>
