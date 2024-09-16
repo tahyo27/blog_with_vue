812f46bd2a9e3b97a75d54c954aa9a3375137e58
tahyo27 <tahyo27@gmail.com>
2024-09-16 23:07:36 +0900

    홈화면 css 수정 및 포스트 더보기 커서페이징 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,8 @@ import { RouterLink, RouterView } from 'vue-router'
 import { ref, onMounted, computed } from 'vue';
 import axios from 'axios'
 // 타이틀 제목도 길면 자를 생각 하기
-const posts = ref([])
+const posts = ref([]);
+const cursor = ref(null); //더보기용 커서
 
 const latestPost = computed(() => {
   return posts.value.length > 0 ? posts.value[0] : null;
@@ -58,16 +59,44 @@ const postDivide = (content) => {
   return { imageHtml, text: truncatedText };
 };
 
-onMounted(async () => {
-  try {
-    const response = await axios.get('http://localhost:8072/posts')
-    posts.value = response.data
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error fetching posts:', error)
-  }
+// onMounted(async () => {
+//   try {
+//     const response = await axios.get('http://localhost:8072/posts')
+//     posts.value = response.data
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error('Error fetching posts:', error)
+//   }
  
-})
+// })
+
+// 최초 로드 시 게시물 가져오기
+onMounted(async () => {
+  await loadPosts();
+});
+
+// API 호출 함수
+const loadPosts = async () => {
+  try {
+    const response = await axios.get('http://localhost:8072/posts', {
+      params: { cursor: cursor.value }  //커서 페이징 커서 전달
+    });
+    const fetchedPosts = response.data;
+
+    if (fetchedPosts.length > 0) {
+      posts.value = [...posts.value, ...fetchedPosts];  // 기존 포스트에 새 포스트 추가
+      cursor.value = fetchedPosts[fetchedPosts.length - 1].id;  // 마지막 포스트 ID 커서로 업데이트
+    }
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
+};
+
+// 더보기 버튼 클릭 시
+const loadMore = async () => {
+  await loadPosts();
+};
+
 
 
 </script>
@@ -82,42 +111,45 @@ onMounted(async () => {
         <p>{{ truncatedContent }}</p>
       </div>
     </div>
+    
   </main>
   <main class="maintwo">
-    <div class="main-left">
-      <div class="maintwoTrain">
-        <h1>TRAIN OF THOUGHT</h1>
-      </div>
-    <ul>
-      <li v-for="post in remainingPosts" :key="post.id">
-        <div class="image-container" v-html="postDivide(post.content).imageHtml"></div>
-        <div class="text-container">
-          <h1>{{ post.title }}</h1>
-          <p>{{ postDivide(post.content).text }}</p>
+      <div class="main-left">
+        <div class="maintwoTrain">
+          <h1>TRAIN OF THOUGHT</h1>
         </div>
-      </li>
-    </ul>
-  </div>
-    
-  <div class="main-right">
-    <div class="right-about">
-      <h1>ABOUT ME</h1>
-      <div><img src="../../public/고라파덕.webp"></div>
-      <div><p>TH의 블로그입니다. 만들면서 여러 공부를 하는 블로그입니다.</p>
-      </div>
-      <div class="right-read">
-        <RouterLink to="/about">Read More ></RouterLink>
-      </div>     
-    </div>
-  
-    <div class="right-about-bottom">
-      <h1>MY PICK OF MONTH</h1>
-      <div><img src="../../public/book.jpg"></div>
-      <div><p>이번 달 픽은 없습니다.</p></div>
-    </div>
+      <ul>
+        <li v-for="post in remainingPosts" :key="post.id">
+          <div class="image-container" v-html="postDivide(post.content).imageHtml"></div>
+          <div class="text-container">
+            <h1>{{ post.title }}</h1>
+            <p>{{ postDivide(post.content).text }}</p>
+          </div>
+        </li>
+      </ul>
 
-  </div>
+        <button class="plus-button" @click="loadMore">테스트 버튼</button>
+      </div>
+    
+    <div class="main-right">
+        <div class="right-about">
+          <h1>ABOUT ME</h1>
+          <div><img src="../../public/고라파덕.webp"></div>
+          <div><p>TH의 블로그입니다. 만들면서 여러 공부를 하는 블로그입니다.</p>
+          </div>
+          <div class="right-read">
+            <RouterLink to="/about">Read More ></RouterLink>
+          </div>     
+        </div>
+    
+        <div class="right-about-bottom">
+          <h1>MY PICK OF MONTH</h1>
+          <div><img src="../../public/book.jpg"></div>
+          <div><p>이번 달 픽은 없습니다.</p></div>
+        </div>
+    </div>
       
+    
   </main>
 
 </div>
